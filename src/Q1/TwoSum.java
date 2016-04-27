b4f6932c6268d8a5c8e6bc829c79a5dc94f93005
lijinghao <877307770@qq.com>
2016-04-27 20:51:30 +0800

    Q1

---
+++ src/Q1/TwoSum.java
@@ -0,0 +1,30 @@
+package Q1;
+
+public class TwoSum {
+	public static int[] twoSum(int[] nums, int target) {
+        if(null == nums) return null;
+        
+        for(int first=0, second=first+1; first<nums.length-1; first++){
+        	for(;second<nums.length;second++){
+        		if(target == nums[first] + nums[second]){
+        			int[] result = new int[2];
+        			result[0] = first;
+        			result[1] = second;
+        			return result;
+        		}
+        	}
+        }
+        return null;
+    }
+	
+	public static void main(String[] args){
+		int[] nums = {2, 7, 11, 15};
+		int target = 9;
+		
+		int[] result = twoSum(nums, target);
+		if(null != result){
+			System.out.println(result[0]+","+result[1]);
+		}
+		
+	}
+}
